4a2902cafe6f26ea0cf8b33333bcadbfb1de9bf9
francesco.fiorenza@gmail.com <Francesco@Developers-MacBook-Pro.local>
2019-01-13 12:16:49 +1100

    Dist build

--- src/app/photo.service.ts
+++ src/app/photo.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+var api_uri = "http://http://localhost:3000/"; //https://flickr-api.azurewebsites.net
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,10 +11,10 @@ export class PhotoService {
   constructor(private http:HttpClient) { }
 
   getPublicPhotos() {
-    return this.http.get('http://localhost:3000/photos/public')
+    return this.http.get(`${api_uri}/photos/public`)
   }
 
   searchPublicPhotos(searchString:string) {
-    return this.http.post('http://localhost:3000/photos/search', {searchString})
+    return this.http.post(`${api_uri}/photos/search`, {searchString})
   }
 }
